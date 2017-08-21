70c2b571ce303daae06b252cd78c4fca407286b8
gmkseta <gmkseta@gmail.com>
2017-08-21 16:40:55 +0900

    그냥 코딩 해보기

--- sim_week_1/sim_week_1/main.cpp
+++ sim_week_1/sim_week_1/main.cpp
@@ -1,7 +1,29 @@
 //first main function 
 #include <stdio.h>
+#include <iostream>
+
+namespace Vector2D {
+	int x;
+	int y;
+
+	void print()
+	{
+		printf("%d %d", x, y);
+	}
+}
+
 
 void main()
 {
-	printf("Hd");
+	Vector2D::x = 1;
+	Vector2D::y = 2;
+	//Vector2D::print();
+	/*printf("THis is c Style\n");
+	std::cout << "This is c++ style"<<std::endl;
+*/
+	using namespace std;
+	using namespace Vector2D;
+
+	cout << x << " , " << y << endl;
+
 }
