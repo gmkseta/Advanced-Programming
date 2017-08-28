8f435a6db5308e56d9d3310530fb7779174a5a3c
gmkseta <gmkseta@gmail.com>
2017-08-28 13:30:29 +0900

    원 그린거로 파이 계산하기

--- monte/monte/소스.cpp
+++ monte/monte/소스.cpp
@@ -4,19 +4,33 @@
 
 double getRandomNumber(const double min, const double max);
 bool isInsideCircle(const double x, const double y);
+
+
 void main()
 {
 	FILE *of = fopen("square.txt", "w");
 
 	srand((unsigned int)time(NULL));
-	for (int i = 0; i < 10000; i++) {
+
+	int num_pts_circle = 0;
+
+	const int num_pts_square = 1000;
+
+	for (int i = 0; i < num_pts_square; i++) {
+
 		double x = getRandomNumber(0.0, 1.0);
+
 		double y = getRandomNumber(0.0, 1.0);
 
-		if(isInsideCircle(x,y)==true)
+		if (isInsideCircle(x, y) == true)
+		{
+			num_pts_circle++;
 			fprintf(of, "%f, %f\n", x, y);
+		}
 
 	}
+	const double pi_mc = 4.0 * num_pts_circle / num_pts_square;
+	printf("Estimate PI with %d radom pts is %f\n", num_pts_square, pi_mc);
 	fclose(of);
 }
 
