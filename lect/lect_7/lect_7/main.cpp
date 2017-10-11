133ded57f7cc26e67e6911001eafce7fe50277de
gmkseta <gmkseta@gmail.com>
2017-10-11 11:52:59 +0900

    음 으므흠

--- lect/lect_7/lect_7/main.cpp
+++ lect/lect_7/lect_7/main.cpp
@@ -2,38 +2,58 @@
 
 using namespace std;
 
-int addition(int a, int b)
+class Animal
 {
-	return a + b;
-}
-
-
-int substraction(int a, int b)
+public:
+	virtual void speak() = 0;
+	//나는 이 포인터 위치에 speak가 들어올꺼야앙
+	//이자리에서 찾으면 된다 @@@ 라고하는거임
+};
+class Dog:public Animal
 {
-	return a - b;
-}
+public:
+	void speak()
+	{
+		cout << "Bow" << endl;
+	}
 
-int doSomething(int a, int b , int (*functocall)(int,int))
+};
+class Elephant :public Animal
 {
-	return (*functocall)(1, 2);
-}
+public:
+	void speak()
+	{
+		cout << "Mooo" << endl;
+	}
+};
+class Human :public Animal
+{
+public:
+	void speak()
+	{
+		cout << "da" << endl;
+
+	}
+};
 int main()
 {
-	int(*func_ptr)(int, int) = nullptr;//함수 포인터.
+	Animal *my_dog = new Dog;
+	Animal *my_ele = new Elephant;
+	Animal *my_human = new Human;
 
-	func_ptr = addition;
-	cout << addition(1, 2)<<endl;
-	cout<<(*func_ptr)(1, 2)<<endl;
-	func_ptr = substraction;
-	cout << substraction(1, 2) << endl;
-	cout << (*func_ptr)(1, 2) << endl;
+	Animal **animal_arr = new Animal*[3];
+	//Animal의 포인터의 arr
 
-	cout << doSomething(1, 2, addition) << endl;
+	//어떤 클래스의 주소를 받을껀지 명시해야함
+	//같은 이름의 함수라도 어디있는지 클래스마다 다를수있기때문에,
+	//클래스 시작점 대비 얘의 주소가 일정하면 됨 (상대주소로? )
+	animal_arr[0] = new Dog;
+	animal_arr[1] = new Elephant;
+	animal_arr[2] = new Human;
 
-	int (*func_ptr_arr[])(int, int) = {addition,substraction};
-	
-	for (int i = 0; i < 2; i++)
-		cout << (*func_ptr_arr[i])(4, 5) << endl;
+	for (int i = 0; i < 3; i++)
+		animal_arr[i]->speak();
+
 
 	return 0;
 }
