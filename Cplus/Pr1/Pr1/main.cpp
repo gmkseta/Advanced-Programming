9bbefece0429326ed264a5b39b817add90cfc5de
gmkseta <gmkseta@gmail.com>
2017-10-13 16:30:17 +0900

    구조체 new delete 연산, const 반환

--- Cplus/Pr1/Pr1/main.cpp
+++ Cplus/Pr1/Pr1/main.cpp
@@ -2,21 +2,27 @@
 
 using namespace std;
 
-typedef struct __Point
-{
-	int xpos;
-	int ypos;
-}Point;
+//
+//3
+//typedef struct __Point
+//{
+//	int xpos;
+//	int ypos;
+//}Point;
+//
+//Point& PntAdder(const Point &p1, const Point &p2);
 
-Point& PntAdder(const Point &p1, const Point &p2);
 int main(void)
 {
-	/*int val1, val2;
+	/*1
+	int val1, val2;
 	cin >> val1 >> val2;
 
 	cout << val1 + "+" + val2<<endl;
 */
-/*
+
+
+/*2
 	const int num = 12;
 
 	const int *hi = &num;
@@ -25,9 +31,37 @@ int main(void)
 
 	cout << num << " " << *hi << " " << *hiref << endl;*/
 
+/*3
+	Point *p1 = new Point;
+	Point *p2 = new Point;
+	p1->xpos = 1;
+	p1->ypos = 2;
+	p2->xpos = 3;
+	p2->ypos = 4;
+
+	
+
+	Point &pref = PntAdder(*p1, *p2);
+
+	cout << pref.xpos << "," << pref.ypos << endl;
+
+	delete p1;
+	delete p2;
+*/
+
+
 
 
 
 }
-
-Point& PntAdder(const Point &p1, const Point &p2);
+//3
+//Point& PntAdder(const Point &p1, const Point &p2)
+//{
+//	Point *pptr = new Point;
+//	pptr->xpos = p1.xpos + p2.xpos;
+//	pptr->ypos = p1.ypos + p2.ypos;
+//
+//	return *pptr;
+//
+//
+//}
